b2d6e0cdfcad598bceb93fcd44d03261f581d869
Veljko <veljkoBitno@gmail.com>
2023-06-02 13:31:34 +0200

    Create filters, unoptimizied version

--- src/components/Jobs.tsx
+++ src/components/Jobs.tsx
@@ -1,6 +1,7 @@
-import { SetStateAction, useCallback, useEffect, useState } from "react";
+import { SetStateAction, useEffect } from "react";
 import { Data, FilterTypes } from "../Data";
 import Job from "./Job";
+import { JobData } from "../Data";
 
 type JobsProps = {
   data: Data | undefined;
@@ -14,12 +15,38 @@ const Jobs = ({ data, setData, filters, getData }: JobsProps) => {
     getData();
   }, [getData]);
 
+  const searchFilter = (job: JobData) => {
+    if (!filters.search) {
+      return job;
+    }
+    return job.position.toLowerCase().includes(filters.search);
+  };
+
+  const fullTimeFilter = (job: JobData) => {
+    if (!filters.fullTime) {
+      return job;
+    }
+
+    return job.contract === "Full Time";
+  };
+
+  const locationFilter = (job: JobData) => {
+    if (!filters.location) {
+      return job;
+    }
+
+    return job.location.toLowerCase().includes(filters.location.toLowerCase());
+  };
   return (
     <div className="mt-[57px] grid gap-12">
       {data &&
-        data.map((job) => {
-          return <Job key={job.id} job={job} />;
-        })}
+        data
+          .filter((job) => searchFilter(job))
+          .filter((job) => fullTimeFilter(job))
+          .filter((job) => locationFilter(job))
+          .map((job) => {
+            return <Job key={job.id} job={job} />;
+          })}
     </div>
   );
 };
